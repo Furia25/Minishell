cebc9a4e3e649ce33bd51bf682eeea3af24f6c41
furia25 <val@ValPc.>
2025-04-09 02:09:17 +0200

    Cleaned wrong commit

--- obj/main.d
+++
@@ -1,9 +0,0 @@
-obj/main.o: src/main.c includes/minishell.h libs/libft/libft.h \
- includes/hashmap.h includes/garbage_collector.h includes/wildcards.h \
- libs/libft/ft_printf.h
-includes/minishell.h:
-libs/libft/libft.h:
-includes/hashmap.h:
-includes/garbage_collector.h:
-includes/wildcards.h:
-libs/libft/ft_printf.h:
