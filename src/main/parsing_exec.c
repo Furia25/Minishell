84ab001f1a1596b8d39e684a93d012369214ab2b
alpayet <alexandrepayet3ea@gmail.com>
2025-05-17 02:48:43 +0200

    quoicoubeh

--- src/main/parsing_exec.c
+++ src/main/parsing_exec.c
@@ -3,18 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_exec.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: val <val@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: alpayet <alpayet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 23:37:34 by alpayet           #+#    #+#             */
-/*   Updated: 2025/05/17 02:31:24 by val              ###   ########.fr       */
+/*   Updated: 2025/05/17 02:48:05 by alpayet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void parsing_exec_end(t_AST_node *top_node_ast, t_minishell *data);
+static int	parsing(char *input, t_minishell *data);
+static void exec(t_minishell *data);
 
 void	parsing_exec(char *input, t_minishell *data)
+{
+	if (parsing(input, data) == EXIT_FAILURE)
+		return ;
+	exec(data);
+}
+
+static int	parsing(char *input, t_minishell *data)
 {
 	t_lst		*tokens;
 	t_AST_node	*top_node_ast;
@@ -24,30 +32,31 @@ void	parsing_exec(char *input, t_minishell *data)
 	data->command_tab = NULL;
 	if (create_tokens(&tokens, input, data) == EXIT_FAILURE
 		|| check_syntax_errors(tokens, data) == EXIT_FAILURE)
-		return ;
+		return (EXIT_FAILURE);
 	print_debug_lst(tokens, LEXEME | TYPE, 2,
 		"\ndisplay tokens just after creating it\n");
 	data->command_tab = create_cmd_tab(tokens, data);
 	print_debug_all_cmd(data->command_tab, LEXEME | TYPE, 3,
 		 "\ndisplay command_tab just after creating it\n");
 	if (!handle_all_here_doc(data->command_tab, data))
-		return ;
+		return (EXIT_FAILURE);
 	print_debug_all_cmd(data->command_tab, LEXEME | TYPE, 5
 		,"\ndisplay command_tab after handle here doc\n");
+	return (EXIT_SUCCESS);
+}
+
+static void exec(t_minishell *data)
+{
+	t_leaf		*final;
+	t_AST_node	*top_node_ast;
+
 	if (data->environment_tab)
 		free_chartab(data->environment_tab);
 	data->environment_tab = make_env(&data->environment);
 	if (!data->environment_tab)
 		malloc_error(data);
 	top_node_ast = create_ast(data->command_tab, data);
-	parsing_exec_end(top_node_ast, data);
-}
-
-static void parsing_exec_end(t_AST_node *top_node_ast, t_minishell *data)
-{
-	t_leaf		*final;
-
-	print_debug_ast(top_node_ast, 6, 
+	print_debug_ast(top_node_ast, 6,
 		"\ndisplay AST just after creating it\n");
 	final = evaluate_ast(top_node_ast, data);
 	execute_cmd(final, data);
