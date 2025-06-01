9fbc16da0e2eca1706543f9918f2e4e41d7f5227
Alpayet <alexandrepayet3ea@gmail.com>
2025-06-01 13:53:13 +0200

    fix : The status returned by AND and OR lists is the final status of the last command executed in the list so i stock in a static the final state in function evaluate_op_logical

--- src/exec/evaluate_op_logical.c
+++ src/exec/evaluate_op_logical.c
@@ -6,7 +6,7 @@
 /*   By: alpayet <alpayet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 16:53:58 by alpayet           #+#    #+#             */
-/*   Updated: 2025/05/30 16:54:15 by alpayet          ###   ########.fr       */
+/*   Updated: 2025/06/01 13:38:18 by alpayet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@ t_leaf	*evaluate_op_logical(t_ast_node *node, t_minishell *data)
 {
 	t_leaf	*left_value;
 	t_leaf	*right_value;
+	static int	final_state;
 
 	if (node->s_ope_node.control_operator == AND)
 	{
 		left_value = evaluate_ast(node->s_ope_node.left_node, data);
-		if (execute_cmd(left_value, data) == 0)
+		if (left_value != NULL)
+			final_state = execute_cmd(left_value, data);
+		if (final_state == 0)
 		{
 			right_value = evaluate_ast(node->s_ope_node.right_node, data);
 			return (right_value);
@@ -28,7 +31,9 @@ t_leaf	*evaluate_op_logical(t_ast_node *node, t_minishell *data)
 		return (NULL);
 	}
 	left_value = evaluate_ast(node->s_ope_node.left_node, data);
-	if (execute_cmd(left_value, data) != 0)
+	if (left_value != NULL)
+		final_state = execute_cmd(left_value, data);
+	if (final_state != 0)
 	{
 		right_value = evaluate_ast(node->s_ope_node.right_node, data);
 		return (right_value);
